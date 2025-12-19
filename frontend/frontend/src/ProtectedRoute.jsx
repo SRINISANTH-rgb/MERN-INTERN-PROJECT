c1c3cc71ea288e631bfdd1842a659ce551dbf365
SRINISANTH.R.J <srinirj2006@gmail.com>
2025-12-19 11:24:31 +0530

    Update ProtectedRoute.jsx

--- frontend/frontend/src/ProtectedRoute.jsx
+++ frontend/frontend/src/ProtectedRoute.jsx
@@ -1,8 +1,8 @@
-import { Navigate } from "react-router-dom";
-import { isLoggedIn } from "./utils/auth";
+import { Navigate } from 'react-router-dom';
+import { isAuthenticated } from './utils/auth';
 
 const ProtectedRoute = ({ children }) => {
-  return isLoggedIn() ? children : <Navigate to="/login" />;
+  return isAuthenticated() ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;

a9fa7ecfa56cd5b183be2b42b7df8218f8df086f
SRINISANTH <your@email.com>
2025-12-19 10:04:56 +0530

    Initial commit: E-commerce project with frontend and backend

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+
+
+
+
+const connectDB = require("./db");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// middleware
+app.use(cors(
+    {
+        origin:"http://16.171.254.187:3000",
+        credentials:true
+    }
+));
+app.use(express.json());
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+
+// DB
+connectDB();
+
+// routes
+app.use("/api/auth", authRoutes);
+
+module.exports = app;

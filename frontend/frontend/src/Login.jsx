2a41b4974cc014e8a3cd796b3c5232b84efa1bab
SRINISANTH.R.J <srinirj2006@gmail.com>
2025-12-19 10:27:35 +0530

    Update login API endpoint URL

--- frontend/frontend/src/Login.jsx
+++ frontend/frontend/src/Login.jsx
@@ -18,7 +18,7 @@ export default function Login({ setUser, loadCart }) {
     }
 
     try {
-      const res = await fetch("http://16.16.67.21:5000/api/auth/login", {
+      const res = await fetch("http://13.61.21.254:5000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
